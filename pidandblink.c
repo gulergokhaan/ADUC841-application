b6738d64a2ebba1b1ecf84528b2ec19d3e6aa369
Gokhan Guler <130696750+gulergokhaan@users.noreply.github.com>
2025-04-27 15:45:36 +0300

    Update pidandblink.c

--- pidandblink.c
+++ pidandblink.c
@@ -25,8 +25,8 @@ void Timer_ISR(void);
 
 // Main fonksiyonu  
 void main(void) {
-//    P2 &= ~(1 << 3);  // Port 2.3'ü temizle  
-//    P0 = 0x00;        // Port sifirlama  
+    P2 &= ~(1 << 3);  // Port 2.3'Ã¼ temizle  
+    P0 = 0x00;        // Port sifirlama  
     DAC_Init();       // DAC ayarlarini baslat  
     ADC_Init();       // ADC'yi baslat  
     Timer_Init();     // Timer'i baslat  
@@ -41,33 +41,23 @@ void DAC_Init(void) {
 // ADC ayarlarini yapilandirma  
 void ADC_Init(void) {  
     ADCCON1 = 0xFC; // ADC mod ayarlari  
-    ADCCON2 = 0x00; // ADC0 seçimi  
+    ADCCON2 = 0x00; // ADC0 seÃ§imi  
 }
 
-// Timer ayar fonksiyonu (0.1ms için timer)  
+// Timer ayar fonksiyonu (0.1ms iÃ§in timer)  
 void Timer_Init(void) {  
     TMOD |= 0x01;  // Timer 0, mod 1 (16-bit)  
-    TH0 = 0xFB;    // Timer baslangiç degeri  
-    TL0 = 0xAC;    // Timer baslangiç degeri  
+    TH0 = 0xFB;    // Timer baslangiÃ§ degeri  
+    TL0 = 0xAC;    // Timer baslangiÃ§ degeri  
     TR0 = 1;       // Timer 0'i baslat  
     ET0 = 1;       // Timer 0 kesmesini etkinlestir  
     EA = 1;        // Genel kesmeleri etkinlestir  
 }
 
-//// UART baslatma  
-//void UART_Init(void) {  
-//    SM0 = 0;
-//    SM1 = 1;       // veya SCON |= x50  
-//    TI = 0;
-//    REN = 1;
-//    TMOD |= 0x20;  // Timer 1, mod 2, otomatik yeniden yükleme  
-//    TH1 = 0xDC;    // Baud rate 9600 için (11.0592 MHz osilatör ile)  
-//    TR1 = 1;       // Timer 1'i baslat  
-//}
 
-// ADC0'dan deger okuma  
+// ADC'dan deger okuma  
 unsigned int Read_ADC(unsigned char channel) {
-    ADCCON2 = (ADCCON2 & 0xF0) | (channel & 0x0F); // Kanal seçimi  
+    ADCCON2 = (ADCCON2 & 0xF0) | (channel & 0x0F); 	// Kanal seÃ§imi  
     SCONV = 1; 
     while (SCONV == 1) {};          
     return ((unsigned int)(ADCDATAH & 0X0F) << 8) | (unsigned int)ADCDATAL; 
@@ -77,15 +67,15 @@ unsigned int Read_ADC(unsigned char channel) {
 void Write_DAC0(unsigned int value) {
     if (value > 4095) value = 4095;
     if (value < 0) value = 0;
-    DAC0H = (value >> 8);  // Üst 8 bit  
+    DAC0H = (value >> 8);  // Ãœst 8 bit  
     DAC0L = value;         // Alt 8 bit DAC0'a yazilir  
 }
 
 // PID kontrol fonksiyonu  
 void PID_Control(void) {
     // ADC'den deger okuma  
-    istenen_deger = Read_ADC(7); // ADC'den deger oku  
-    olculen_deger = Read_ADC(2); // ADC'den deger oku  
+    istenen_deger = Read_ADC(7); // ADC7'den deger oku  
+    olculen_deger = Read_ADC(2); // ADC2'den deger oku  
 
     // Hata hesaplama  
     hata = istenen_deger - olculen_deger;
@@ -98,18 +88,18 @@ void PID_Control(void) {
     ui = (float)(Ki * integral);
     oncekiintegraldegeri = integral;
 
-    // Türev terimi  
+    // TÃ¼rev terimi  
     ud = (float)(Kd * (hata - turev));
     turev = hata;
 
-    // PID çikisi  
+    // PID Ã§ikisi  
     kontrol_sinyali = (float)(up + ui + ud);
 
-    // Çikis sinirlandirma  
+    // Ã‡ikis sinirlandirma  
     if (kontrol_sinyali > 4095) kontrol_sinyali = 4095;
     if (kontrol_sinyali < 0) kontrol_sinyali = 0;
 
-    // PID çikisini DAC’a yaz  
+    // PID Ã§ikisini DACâ€™a yaz  
     Write_DAC0(kontrol_sinyali);
 }
 
@@ -119,11 +109,11 @@ void Timer_ISR(void) interrupt 1 {
     counter++;
  
 
-    if (counter == 1150) {  // Örnekleme zamani: 0.1ms * 1150 = 115ms  
+    if (counter == 1150) {  // Ã–rnekleme zamani: 0.1ms * 1150 = 115ms  
         counter = 0;  
-//        P0 = ~P0;           // Ledleri tersleme islemi  
-        PID_Control();      // PID kontrol fonksiyonunu çagir (yalnizca bu kesmede)  
-			   TH0 = 0xFB;       // Timer baslangiç degeri  
-         TL0 = 0xAC;       // Timer baslangiç degeri  
+	P0 = ~P0;           // Ledleri tersleme islemi  
+        PID_Control();      // PID kontrol fonksiyonunu Ã§agir (yalnizca bu kesmede)  
+	TH0 = 0xFB;       // Timer baslangiÃ§ degeri  
+        TL0 = 0xAC;       // Timer baslangiÃ§ degeri  
     }
 }
